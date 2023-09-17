afc2470acf2d4f70baf5dc1a31279887bbb01f1b
Rhonald Reyes <rhonaldr1990@gmail.com>
2023-09-17 14:33:43 -0300

    second_hw

--- M04_flujosdecontrol/Prep_Course_Homework_04.py
+++ M04_flujosdecontrol/Prep_Course_Homework_04.py
@@ -64,7 +64,13 @@ for a in range(num2):
 # 6) Utilizar un ciclo while para realizar el factoreo de un número guardado en una variable, sólo si la variable contiene un número entero mayor a 0
 
 # In[33]:
-
+num3 = 5
+if type(num3) == int and num3 > 0:
+    num4 = num3
+    while (num3 > 1):   
+        num3 = num3 -1
+        num4 = num4 * num3
+    print(f"El facorial es {num4}")
 
 
 
@@ -72,7 +78,13 @@ for a in range(num2):
 # 7) Crear un ciclo for dentro de un ciclo while
 
 # In[38]:
-
+#Para este ejercicio me he planteado como reto hacer un ciclo que sume todos los numeros pares hasta 10
+num5 = 0
+while (num5 < 11):
+    for num6 in range(1,11):
+        if num6 % 2 == 0:
+            num5 = num5 + num6
+    print("La suma de todos los pares del 1 al 10 es", num5)
 
 
 
@@ -80,7 +92,12 @@ for a in range(num2):
 # 8) Crear un ciclo while dentro de un ciclo for
 
 # In[3]:
-
+n = 5
+for i in range(1, n):
+    while(n < 5):
+        n -= 1
+    print('Ciclo while nro ' + str(n))
+    print('Ciclo for nro ' + str(i))
 
 
 
@@ -88,34 +105,83 @@ for a in range(num2):
 # 9) Imprimir los números primos existentes entre 0 y 30
 
 # In[54]:
-
-
+n = 0
+while (n < 30):
+    esPrimo = True
+    for i in range (2, n):
+        if (n % i == 0):
+            esPrimo = False
+    if (esPrimo == True):
+        print(n)
+    n += 1
 
 
 # 10) ¿Se puede mejorar el proceso del punto 9? Utilizar las sentencias break y/ó continue para tal fin
 
 # In[55]:
-
-
+n = 0
+while (n < 30):
+    esPrimo = True
+    for i in range (2, n):
+        if (n % i == 0):
+            esPrimo = False
+            break
+    if (esPrimo == True):
+        print(n)
+    n += 1
 
 
 
 # 11) En los puntos 9 y 10, se diseño un código que encuentra números primos y además se lo optimizó. ¿Es posible saber en qué medida se optimizó?
 
 # In[56]:
-
+#este no lo etendí así que solo copié y pegué.
+ciclos_sin_break = 0
+n = 0
+primo = True
+while (n < 30):
+    for div in range(2, n):
+        ciclos_sin_break += 1
+        if (n % div == 0):
+            primo = False
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_sin_break))
 
 
 
 # In[57]:
-
+ciclos_con_break = 0
+n = 0
+primo = True
+while (n < 30):
+    for div in range(2, n):
+        ciclos_con_break += 1
+        if (n % div == 0):
+            primo = False
+            break
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_con_break))
+print('Se optimizó a un ' + str(ciclos_con_break/ciclos_sin_break) + '% de ciclos aplicando break')
 
 
 
 # 12) Aplicando continue, armar un ciclo while que solo imprima los valores divisibles por 12, dentro del rango de números de 100 a 300
 
 # In[62]:
-
+n = 99
+while (n <= 300):
+    n += 1
+    if (n % 12 != 0):
+        continue
+    print(n)
 
 
 
@@ -123,13 +189,21 @@ for a in range(num2):
 # 13) Utilizar la función **input()** que permite hacer ingresos por teclado, para encontrar números primos y dar la opción al usario de buscar el siguiente
 
 # In[73]:
-
+while (n <= 300):
+    if(n % 6 == 0):
+        print(n)
+        break
+    n += 1    
 
 
 
 # 14) Crear un ciclo while que encuentre dentro del rango de 100 a 300 el primer número divisible por 3 y además múltiplo de 6
 
 # In[75]:
-
+while (n <= 300):
+    if(n % 6 == 0):
+        print(n)
+        break
+    n += 1    
 
 
